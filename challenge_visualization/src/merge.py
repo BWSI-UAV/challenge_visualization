9511f987e68ff1bf9398b6a52ba5b5efec5e6992
Drone <bwsiuav+phantom@gmail.com>
2019-08-01 14:14:35 -0400

    augmented_reality_arrow

--- challenge_visualization/src/merge.py
+++ challenge_visualization/src/merge.py
@@ -6,7 +6,7 @@ import threading
 from std_msgs.msg import String
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
-
+from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
 class Imagemerge:
     def __init__(self):
         # Create node with name 'merge'
@@ -19,12 +19,25 @@ class Imagemerge:
 	self.downward_line_sub = rospy.Subscriber('/line/detector_image',Image, self.downward_line_sub_cb)
         # create local variables for the drone
         self.master_image = np.zeros((360,360, 3), np.uint8)
-	self.sub_image = np.zeros((360,480,3),np.uint16)
+	#self.sub_image = np.zeros((360,480,3),np.uint16)
         # A publisher which will publish an image annotated with the detected line to the topic 'line/detector_image'
         self.converter_image_pub = rospy.Publisher('/visualization/converter_image', Image, queue_size=1)
-
+	self.ar_pose_sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.ar_pose_cb)
+	self.upcycle = 0
+	self.downcycle= 0
+	self.showup = False
+	self.showdown = False
+	
         # Initialize instance of CvBridge to convert images between OpenCV images and ROS images
         self.bridge = CvBridge()
+    def ar_pose_cb(self,msg):
+	if len(msg.markers)<1:
+		return
+	marker = min(msg.markers, key=lambda p:p.pose.pose.position.z)
+	if marker.id%2==0:
+		self.showup = True
+	if marker.id%2==1:
+		self.showdown = True
     def downward_line_sub_cb(self,img):
 	converted_img = self.bridge.imgmsg_to_cv2(img, "8UC3")
 	self.master_image[180:360, 180:360,:] = cv2.resize(converted_img, (180,180))
@@ -33,7 +46,32 @@ class Imagemerge:
         """ Add the forward color image to a portion of the master image. """
         converted_img = self.bridge.imgmsg_to_cv2(img, "8UC3") # convert from ROS to cv2 images.
         self.master_image[:180, :360, :] = cv2.resize(converted_img, (360,180)) # downsize the image
-
+	if self.showup == True and self.upcycle<30:
+		pt1 = (180, 45 - self.upcycle)
+		pt2 = (210, 90 - self.upcycle)
+		pt3 = (150, 90 - self.upcycle)
+		p1 = (170, 90 - self.upcycle)
+		p2 = (190, 150 - self.upcycle)
+		triangle = np.array([pt1,pt2,pt3])
+		cv2.drawContours(self.master_image,[triangle],0,(255,0,0), -1)
+		cv2.rectangle(self.master_image,p1,p2, (255,0,0), -1)
+		self.upcycle = self.upcycle+1
+	if self.showup == True and self.upcycle>=30:
+		self.showup = False
+		self.upcycle = 0
+	if self.showdown == True and self.downcycle<30:
+		pt1= (180, 135 + self.downcycle)
+		pt2 =(210, 90 + self.downcycle)
+		pt3 =(150, 90 + self.downcycle)
+		p1 = (170, 30+self.downcycle)
+		p2= (190, 90+self.downcycle)
+		triangle = np.array([pt1,pt2,pt3])
+		cv2.drawContours(self.master_image,[triangle], 0, (255,0,0),-1)
+		cv2.rectangle(self.master_image,p1,p2, (255,0,0),-1)
+		self.downcycle = self.downcycle+1
+	if self.showdown == True and self.downcycle>=30:
+		self.showdown = False
+		self.downcycle = 0
     #def forward_depth_sub_cb(self, img):
      #   "Add the forward depth camera to a portion of the master image."
       #  converted_img = self.bridge.imgmsg_to_cv2(img, "16UC1") # convert from ROS to cv2 images.
